Stichting-MINIX-Research-Foundation/minix

libi2cdriver: add functions for IC register access
Many i2c device drivers used similar code to access registers on
the ICs they drive. This commit implements that functionality in
libi2cdriver and updates the drivers to use the library instead of
their own register access functions. The net result is 375+ fewer
lines of code and less work for people developing new drivers.

The two exceptions were cat24c256 and parts of tda19988. They access
the bus in uncommon ways. It doesn't make sense at this time to
move their read/write functions into libi2cdriver.

Change-Id: Id8280b71af33b710a49944d7f20a7262be9f5988

drivers/tps65950/rtc.c
@@ -26,13 +26,14 @@ rtc_init(void)
 	uint8_t val;
 	struct tm t;
 
-	r = reg_set(ID4, RTC_CTRL_REG, (1 << STOP_RTC_BIT));
+	r = i2creg_set8(bus_endpoint, addresses[ID4], RTC_CTRL_REG,
+	    (1 << STOP_RTC_BIT));
 	if (r != OK) {
 		log_warn(&log, "Failed to start RTC\n");
 		return -1;
 	}
 
-	r = reg_read(ID4, RTC_STATUS_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], RTC_STATUS_REG, &val);
 	if (r != OK) {
 		log_warn(&log, "Failed to read RTC_STATUS_REG\n");
 		return -1;
@@ -59,7 +60,8 @@ rtc_get_time(struct tm *t, int flags)
 	/* Write GET_TIME_BIT to RTC_CTRL_REG to latch the RTC values into
 	 * the RTC registers. This is required before each read.
 	 */
-	r = reg_set(ID4, RTC_CTRL_REG, (1 << GET_TIME_BIT));
+	r = i2creg_set8(bus_endpoint, addresses[ID4], RTC_CTRL_REG,
+	    (1 << GET_TIME_BIT));
 	if (r != OK) {
 		return -1;
 	}
@@ -67,42 +69,42 @@ rtc_get_time(struct tm *t, int flags)
 	/* Read and Convert BCD to binary (default RTC mode). */
 
 	/* Seconds - 0 to 59 */
-	r = reg_read(ID4, SECONDS_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], SECONDS_REG, &val);
 	if (r != OK) {
 		return -1;
 	}
 	t->tm_sec = bcd_to_dec(val & 0x7f);
 
 	/* Minutes - 0 to 59 */
-	r = reg_read(ID4, MINUTES_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], MINUTES_REG, &val);
 	if (r != OK) {
 		return -1;
 	}
 	t->tm_min = bcd_to_dec(val & 0x7f);
 
 	/* Hours - 0 to 23 */
-	r = reg_read(ID4, HOURS_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], HOURS_REG, &val);
 	if (r != OK) {
 		return -1;
 	}
 	t->tm_hour = bcd_to_dec(val & 0x3f);
 
 	/* Days - 1 to 31 */
-	r = reg_read(ID4, DAYS_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], DAYS_REG, &val);
 	if (r != OK) {
 		return -1;
 	}
 	t->tm_mday = bcd_to_dec(val & 0x3f);
 
 	/* Months - Jan=1 to Dec=12 */
-	r = reg_read(ID4, MONTHS_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], MONTHS_REG, &val);
 	if (r != OK) {
 		return -1;
 	}
 	t->tm_mon = bcd_to_dec(val & 0x1f) - 1;
 
 	/* Years - last 2 digits of year */
-	r = reg_read(ID4, YEARS_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], YEARS_REG, &val);
 	if (r != OK) {
 		return -1;
 	}
@@ -129,32 +131,38 @@ rtc_set_time(struct tm *t, int flags)
 	int r;
 
 	/* Write the date/time to the RTC registers. */
-	r = reg_write(ID4, SECONDS_REG, (dec_to_bcd(t->tm_sec) & 0x7f));
+	r = i2creg_write8(bus_endpoint, addresses[ID4], SECONDS_REG,
+	    (dec_to_bcd(t->tm_sec) & 0x7f));
 	if (r != OK) {
 		return -1;
 	}
 
-	r = reg_write(ID4, MINUTES_REG, (dec_to_bcd(t->tm_min) & 0x7f));
+	r = i2creg_write8(bus_endpoint, addresses[ID4], MINUTES_REG,
+	    (dec_to_bcd(t->tm_min) & 0x7f));
 	if (r != OK) {
 		return -1;
 	}
 
-	r = reg_write(ID4, HOURS_REG, (dec_to_bcd(t->tm_hour) & 0x3f));
+	r = i2creg_write8(bus_endpoint, addresses[ID4], HOURS_REG,
+	    (dec_to_bcd(t->tm_hour) & 0x3f));
 	if (r != OK) {
 		return -1;
 	}
 
-	r = reg_write(ID4, DAYS_REG, (dec_to_bcd(t->tm_mday) & 0x3f));
+	r = i2creg_write8(bus_endpoint, addresses[ID4], DAYS_REG,
+	    (dec_to_bcd(t->tm_mday) & 0x3f));
 	if (r != OK) {
 		return -1;
 	}
 
-	r = reg_write(ID4, MONTHS_REG, (dec_to_bcd(t->tm_mon + 1) & 0x1f));
+	r = i2creg_write8(bus_endpoint, addresses[ID4], MONTHS_REG,
+	    (dec_to_bcd(t->tm_mon + 1) & 0x1f));
 	if (r != OK) {
 		return -1;
 	}
 
-	r = reg_write(ID4, YEARS_REG, (dec_to_bcd(t->tm_year % 100) & 0xff));
+	r = i2creg_write8(bus_endpoint, addresses[ID4], YEARS_REG,
+	    (dec_to_bcd(t->tm_year % 100) & 0xff));
 	if (r != OK) {
 		return -1;
 	}

lib/libi2cdriver/i2cdriver.c
@@ -1,5 +1,6 @@
 /* This file contains device independent i2c device driver helpers. */
 
+#include <assert.h>
 #include <minix/drivers.h>
 #include <minix/endpoint.h>
 #include <minix/i2c.h>
@@ -192,4 +193,175 @@ i2cdriver_exec(endpoint_t bus_endpoint, minix_i2c_ioctl_exec_t * ioctl_exec)
 	}
 
 	return m.REP_STATUS;
+}
+
+static int
+__i2creg_read(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t raw,
+    uint8_t reg, uint32_t * val, size_t vallen)
+{
+	int r, i;
+	minix_i2c_ioctl_exec_t ioctl_exec;
+
+	assert(val != NULL);
+	assert(vallen >= 1 && vallen <= 4);
+
+	memset(&ioctl_exec, '\0', sizeof(minix_i2c_ioctl_exec_t));
+
+	/* Read from chip */
+	ioctl_exec.iie_op = I2C_OP_READ_WITH_STOP;
+	ioctl_exec.iie_addr = address;
+
+	if (!raw) {
+		/* write the register address */
+		ioctl_exec.iie_cmd[0] = reg;
+		ioctl_exec.iie_cmdlen = 1;
+	}
+
+	/* read vallen bytes */
+	ioctl_exec.iie_buflen = vallen;
+
+	r = i2cdriver_exec(bus_endpoint, &ioctl_exec);
+	if (r != OK) {
+		return -1;
+	}
+
+	for (*val = 0, i = 0; i < vallen; i++) {
+		*val = ((*val) << 8) | ioctl_exec.iie_buf[i];
+	}
+
+	return OK;
+}
+
+int
+i2creg_raw_read8(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t * val)
+{
+	int r;
+	uint32_t val32;
+
+	r = __i2creg_read(bus_endpoint, address, 1, 0, &val32, 1);
+	*val = val32 & 0xff;
+
+	return r;
+}
+
+int
+i2creg_read8(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t reg,
+    uint8_t * val)
+{
+	int r;
+	uint32_t val32;
+
+	r = __i2creg_read(bus_endpoint, address, 0, reg, &val32, 1);
+	*val = val32 & 0xff;
+
+	return r;
+}
+
+int
+i2creg_read16(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t reg,
+    uint16_t * val)
+{
+	int r;
+	uint32_t val32;
+	
+	r = __i2creg_read(bus_endpoint, address, 0, reg, &val32, 2);
+	*val = val32 & 0xffff;
+	
+	return r;
+}
+
+int
+i2creg_read24(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t reg,
+    uint32_t * val)
+{
+	return __i2creg_read(bus_endpoint, address, 0, reg, val, 3);
+}
+
+static int
+__i2creg_write(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t raw,
+    uint8_t reg, uint8_t val)
+{
+	int r;
+	minix_i2c_ioctl_exec_t ioctl_exec;
+
+	memset(&ioctl_exec, '\0', sizeof(minix_i2c_ioctl_exec_t));
+
+	/* Write to chip */
+	ioctl_exec.iie_op = I2C_OP_WRITE_WITH_STOP;
+	ioctl_exec.iie_addr = address;
+
+	if (raw) {
+		/* write just the value */
+		ioctl_exec.iie_buf[0] = val;
+		ioctl_exec.iie_buflen = 1;
+	} else {
+		/* write the register address and value */
+		ioctl_exec.iie_buf[0] = reg;
+		ioctl_exec.iie_buf[1] = val;
+		ioctl_exec.iie_buflen = 2;
+	}
+
+	r = i2cdriver_exec(bus_endpoint, &ioctl_exec);
+	if (r != OK) {
+		return -1;
+	}
+
+	return OK;
+}
+
+int
+i2creg_write8(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t reg,
+    uint8_t val)
+{
+	return __i2creg_write(bus_endpoint, address, 0, reg, val);
+}
+
+int
+i2creg_raw_write8(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t val)
+{
+	return __i2creg_write(bus_endpoint, address, 1, 0, val);
+}
+
+int
+i2creg_set8(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t reg,
+    uint8_t mask)
+{
+	int r;
+	uint8_t val;
+
+	r = i2creg_read8(bus_endpoint, address, reg, &val);
+	if (r != OK) {
+		return -1;
+	}
+
+	val |= mask;
+
+	r = i2creg_write8(bus_endpoint, address, reg, val);
+	if (r != OK) {
+		return -1;
+	}
+
+	return OK;
+}
+
+int
+i2creg_clear8(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t reg,
+    uint8_t mask)
+{
+	int r;
+	uint8_t val;
+
+	r = i2creg_read8(bus_endpoint, address, reg, &val);
+	if (r != OK) {
+		return -1;
+	}
+
+	val &= ~mask;
+
+	r = i2creg_write8(bus_endpoint, address, reg, val);
+	if (r != OK) {
+		return -1;
+	}
+
+	return OK;
 }

JBouron/Minix3-Fine-Grain

drivers/tps65950/rtc.c
@@ -26,13 +26,14 @@ rtc_init(void)
 	uint8_t val;
 	struct tm t;
 
-	r = reg_set(ID4, RTC_CTRL_REG, (1 << STOP_RTC_BIT));
+	r = i2creg_set_bits8(bus_endpoint, addresses[ID4], RTC_CTRL_REG,
+	    (1 << STOP_RTC_BIT));
 	if (r != OK) {
 		log_warn(&log, "Failed to start RTC\n");
 		return -1;
 	}
 
-	r = reg_read(ID4, RTC_STATUS_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], RTC_STATUS_REG, &val);
 	if (r != OK) {
 		log_warn(&log, "Failed to read RTC_STATUS_REG\n");
 		return -1;
@@ -59,7 +60,8 @@ rtc_get_time(struct tm *t, int flags)
 	/* Write GET_TIME_BIT to RTC_CTRL_REG to latch the RTC values into
 	 * the RTC registers. This is required before each read.
 	 */
-	r = reg_set(ID4, RTC_CTRL_REG, (1 << GET_TIME_BIT));
+	r = i2creg_set_bits8(bus_endpoint, addresses[ID4], RTC_CTRL_REG,
+	    (1 << GET_TIME_BIT));
 	if (r != OK) {
 		return -1;
 	}
@@ -67,42 +69,42 @@ rtc_get_time(struct tm *t, int flags)
 	/* Read and Convert BCD to binary (default RTC mode). */
 
 	/* Seconds - 0 to 59 */
-	r = reg_read(ID4, SECONDS_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], SECONDS_REG, &val);
 	if (r != OK) {
 		return -1;
 	}
 	t->tm_sec = bcd_to_dec(val & 0x7f);
 
 	/* Minutes - 0 to 59 */
-	r = reg_read(ID4, MINUTES_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], MINUTES_REG, &val);
 	if (r != OK) {
 		return -1;
 	}
 	t->tm_min = bcd_to_dec(val & 0x7f);
 
 	/* Hours - 0 to 23 */
-	r = reg_read(ID4, HOURS_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], HOURS_REG, &val);
 	if (r != OK) {
 		return -1;
 	}
 	t->tm_hour = bcd_to_dec(val & 0x3f);
 
 	/* Days - 1 to 31 */
-	r = reg_read(ID4, DAYS_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], DAYS_REG, &val);
 	if (r != OK) {
 		return -1;
 	}
 	t->tm_mday = bcd_to_dec(val & 0x3f);
 
 	/* Months - Jan=1 to Dec=12 */
-	r = reg_read(ID4, MONTHS_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], MONTHS_REG, &val);
 	if (r != OK) {
 		return -1;
 	}
 	t->tm_mon = bcd_to_dec(val & 0x1f) - 1;
 
 	/* Years - last 2 digits of year */
-	r = reg_read(ID4, YEARS_REG, &val);
+	r = i2creg_read8(bus_endpoint, addresses[ID4], YEARS_REG, &val);
 	if (r != OK) {
 		return -1;
 	}
@@ -129,32 +131,38 @@ rtc_set_time(struct tm *t, int flags)
 	int r;
 
 	/* Write the date/time to the RTC registers. */
-	r = reg_write(ID4, SECONDS_REG, (dec_to_bcd(t->tm_sec) & 0x7f));
+	r = i2creg_write8(bus_endpoint, addresses[ID4], SECONDS_REG,
+	    (dec_to_bcd(t->tm_sec) & 0x7f));
 	if (r != OK) {
 		return -1;
 	}
 
-	r = reg_write(ID4, MINUTES_REG, (dec_to_bcd(t->tm_min) & 0x7f));
+	r = i2creg_write8(bus_endpoint, addresses[ID4], MINUTES_REG,
+	    (dec_to_bcd(t->tm_min) & 0x7f));
 	if (r != OK) {
 		return -1;
 	}
 
-	r = reg_write(ID4, HOURS_REG, (dec_to_bcd(t->tm_hour) & 0x3f));
+	r = i2creg_write8(bus_endpoint, addresses[ID4], HOURS_REG,
+	    (dec_to_bcd(t->tm_hour) & 0x3f));
 	if (r != OK) {
 		return -1;
 	}
 
-	r = reg_write(ID4, DAYS_REG, (dec_to_bcd(t->tm_mday) & 0x3f));
+	r = i2creg_write8(bus_endpoint, addresses[ID4], DAYS_REG,
+	    (dec_to_bcd(t->tm_mday) & 0x3f));
 	if (r != OK) {
 		return -1;
 	}
 
-	r = reg_write(ID4, MONTHS_REG, (dec_to_bcd(t->tm_mon + 1) & 0x1f));
+	r = i2creg_write8(bus_endpoint, addresses[ID4], MONTHS_REG,
+	    (dec_to_bcd(t->tm_mon + 1) & 0x1f));
 	if (r != OK) {
 		return -1;
 	}
 
-	r = reg_write(ID4, YEARS_REG, (dec_to_bcd(t->tm_year % 100) & 0xff));
+	r = i2creg_write8(bus_endpoint, addresses[ID4], YEARS_REG,
+	    (dec_to_bcd(t->tm_year % 100) & 0xff));
 	if (r != OK) {
 		return -1;
 	}

lib/libi2cdriver/i2cdriver.c
@@ -1,5 +1,6 @@
 /* This file contains device independent i2c device driver helpers. */
 
+#include <assert.h>
 #include <minix/drivers.h>
 #include <minix/endpoint.h>
 #include <minix/i2c.h>
@@ -192,4 +193,175 @@ i2cdriver_exec(endpoint_t bus_endpoint, minix_i2c_ioctl_exec_t * ioctl_exec)
 	}
 
 	return m.REP_STATUS;
+}
+
+static int
+__i2creg_read(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t raw,
+    uint8_t reg, uint32_t * val, size_t vallen)
+{
+	int r, i;
+	minix_i2c_ioctl_exec_t ioctl_exec;
+
+	assert(val != NULL);
+	assert(vallen >= 1 && vallen <= 4);
+
+	memset(&ioctl_exec, '\0', sizeof(minix_i2c_ioctl_exec_t));
+
+	/* Read from chip */
+	ioctl_exec.iie_op = I2C_OP_READ_WITH_STOP;
+	ioctl_exec.iie_addr = address;
+
+	if (!raw) {
+		/* write the register address */
+		ioctl_exec.iie_cmd[0] = reg;
+		ioctl_exec.iie_cmdlen = 1;
+	}
+
+	/* read vallen bytes */
+	ioctl_exec.iie_buflen = vallen;
+
+	r = i2cdriver_exec(bus_endpoint, &ioctl_exec);
+	if (r != OK) {
+		return -1;
+	}
+
+	for (*val = 0, i = 0; i < vallen; i++) {
+		*val = ((*val) << 8) | ioctl_exec.iie_buf[i];
+	}
+
+	return OK;
+}
+
+int
+i2creg_raw_read8(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t * val)
+{
+	int r;
+	uint32_t val32;
+
+	r = __i2creg_read(bus_endpoint, address, 1, 0, &val32, 1);
+	*val = val32 & 0xff;
+
+	return r;
+}
+
+int
+i2creg_read8(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t reg,
+    uint8_t * val)
+{
+	int r;
+	uint32_t val32;
+
+	r = __i2creg_read(bus_endpoint, address, 0, reg, &val32, 1);
+	*val = val32 & 0xff;
+
+	return r;
+}
+
+int
+i2creg_read16(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t reg,
+    uint16_t * val)
+{
+	int r;
+	uint32_t val32;
+
+	r = __i2creg_read(bus_endpoint, address, 0, reg, &val32, 2);
+	*val = val32 & 0xffff;
+
+	return r;
+}
+
+int
+i2creg_read24(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t reg,
+    uint32_t * val)
+{
+	return __i2creg_read(bus_endpoint, address, 0, reg, val, 3);
+}
+
+static int
+__i2creg_write(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t raw,
+    uint8_t reg, uint8_t val)
+{
+	int r;
+	minix_i2c_ioctl_exec_t ioctl_exec;
+
+	memset(&ioctl_exec, '\0', sizeof(minix_i2c_ioctl_exec_t));
+
+	/* Write to chip */
+	ioctl_exec.iie_op = I2C_OP_WRITE_WITH_STOP;
+	ioctl_exec.iie_addr = address;
+
+	if (raw) {
+		/* write just the value */
+		ioctl_exec.iie_buf[0] = val;
+		ioctl_exec.iie_buflen = 1;
+	} else {
+		/* write the register address and value */
+		ioctl_exec.iie_buf[0] = reg;
+		ioctl_exec.iie_buf[1] = val;
+		ioctl_exec.iie_buflen = 2;
+	}
+
+	r = i2cdriver_exec(bus_endpoint, &ioctl_exec);
+	if (r != OK) {
+		return -1;
+	}
+
+	return OK;
+}
+
+int
+i2creg_write8(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t reg,
+    uint8_t val)
+{
+	return __i2creg_write(bus_endpoint, address, 0, reg, val);
+}
+
+int
+i2creg_raw_write8(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t val)
+{
+	return __i2creg_write(bus_endpoint, address, 1, 0, val);
+}
+
+int
+i2creg_set_bits8(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t reg,
+    uint8_t bits)
+{
+	int r;
+	uint8_t val;
+
+	r = i2creg_read8(bus_endpoint, address, reg, &val);
+	if (r != OK) {
+		return -1;
+	}
+
+	val |= bits;
+
+	r = i2creg_write8(bus_endpoint, address, reg, val);
+	if (r != OK) {
+		return -1;
+	}
+
+	return OK;
+}
+
+int
+i2creg_clear_bits8(endpoint_t bus_endpoint, i2c_addr_t address, uint8_t reg,
+    uint8_t bits)
+{
+	int r;
+	uint8_t val;
+
+	r = i2creg_read8(bus_endpoint, address, reg, &val);
+	if (r != OK) {
+		return -1;
+	}
+
+	val &= ~bits;
+
+	r = i2creg_write8(bus_endpoint, address, reg, val);
+	if (r != OK) {
+		return -1;
+	}
+
+	return OK;
 }