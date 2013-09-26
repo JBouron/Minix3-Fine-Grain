Stichting-MINIX-Research-Foundation/minix

arm:make the MMU fetch pagetable data through the caches.
Change-Id: Ibd7b66558c369d0c0792c02801562580d255fa1f

kernel/arch/earm/include/cpufunc.h
@@ -215,16 +215,20 @@ static inline u32_t read_ttbr0()
 	asm volatile("mrc p15, 0, %[bar], c2, c0, 0 @ Read TTBR0\n\t"
 			: [bar] "=r" (bar));
 
-	return bar;
+	return bar & TTBR_ADDR_MASK;
 }
 
 /* Write Translation Table Base Register 0 */
 static inline void write_ttbr0(u32_t bar)
 {
 	barrier();
-
+	/* In our setup TTBR contains the base address *and* the flags
+	   but other pieces of the kernel code expect ttbr to be the 
+	   base address of the l1 page table. We therefore add the
+	   flags here and remove them in the read_ttbr0 */
+	u32_t v  =  (bar  & TTBR_ADDR_MASK ) | TTBR_FLAGS_CACHED;
 	asm volatile("mcr p15, 0, %[bar], c2, c0, 0 @ Write TTBR0\n\t"
-			: : [bar] "r" (bar));
+			: : [bar] "r" (v));
 
 	refresh_tlb();
 }

kernel/arch/earm/memory.c
@@ -303,7 +303,7 @@ int vm_lookup(const struct proc *proc, const vir_bytes virtual,
 	assert(HASPT(proc));
 
 	/* Retrieve page directory entry. */
-	root = (u32_t *) proc->p_seg.p_ttbr;
+	root = (u32_t *) (proc->p_seg.p_ttbr & TTBR_ADDR_MASK);
 	assert(!((u32_t) root % ARM_PAGEDIR_SIZE));
 	pde = ARM_VM_PDE(virtual);
 	assert(pde >= 0 && pde < ARM_VM_DIR_ENTRIES);

JBouron/Minix3-Fine-Grain

kernel/arch/earm/include/cpufunc.h
@@ -215,16 +215,20 @@ static inline u32_t read_ttbr0()
 	asm volatile("mrc p15, 0, %[bar], c2, c0, 0 @ Read TTBR0\n\t"
 			: [bar] "=r" (bar));
 
-	return bar;
+	return bar & ARM_TTBR_ADDR_MASK;
 }
 
 /* Write Translation Table Base Register 0 */
 static inline void write_ttbr0(u32_t bar)
 {
 	barrier();
-
+	/* In our setup TTBR contains the base address *and* the flags
+	   but other pieces of the kernel code expect ttbr to be the 
+	   base address of the l1 page table. We therefore add the
+	   flags here and remove them in the read_ttbr0 */
+	u32_t v  =  (bar  & ARM_TTBR_ADDR_MASK ) | ARM_TTBR_FLAGS_CACHED;
 	asm volatile("mcr p15, 0, %[bar], c2, c0, 0 @ Write TTBR0\n\t"
-			: : [bar] "r" (bar));
+			: : [bar] "r" (v));
 
 	refresh_tlb();
 }

kernel/arch/earm/memory.c
@@ -303,7 +303,7 @@ int vm_lookup(const struct proc *proc, const vir_bytes virtual,
 	assert(HASPT(proc));
 
 	/* Retrieve page directory entry. */
-	root = (u32_t *) proc->p_seg.p_ttbr;
+	root = (u32_t *) (proc->p_seg.p_ttbr & ARM_TTBR_ADDR_MASK);
 	assert(!((u32_t) root % ARM_PAGEDIR_SIZE));
 	pde = ARM_VM_PDE(virtual);
 	assert(pde >= 0 && pde < ARM_VM_DIR_ENTRIES);