Stichting-MINIX-Research-Foundation/minix

netbsd dirent.h, import sys/sys *.h, mfs cleanup
	. add all sys/sys headers not already present to help compiling
	. take netbsd dirent.h and struct dirent; main result is
	  introducing d_type and d_namlen that have to be set by getdents()
	  in all FS code implementing it
	. d_off is gone
	. alignment of the struct has become 8 bytes instead of 4
	. remove _MAX_BLOCK_SIZE, _MIN_BLOCK_SIZE, _STATIC_BLOCK_SIZE
	. libminlib: cleanup unused yet duplicate code
	. mfs: throw out the long-broken v1, v2 support
	. new test for dirent contents filled by getdents()

Change-Id: I3b913d276803dad15896b2ea750d7419f32dd224

lib/libvtreefs/read.c
@@ -2,6 +2,7 @@
 
 #include "inc.h"
 #include <dirent.h>
+#include <minix/minlib.h>
 
 #define GETDENTS_BUFSIZ 4096
 #define DWORD_ALIGN(len) (((len) + sizeof(long) - 1) & ~(sizeof(long) - 1))
@@ -161,7 +162,8 @@ int fs_getdents(void)
 			name = child->i_name;
 		}
 
-		len = DWORD_ALIGN(sizeof(struct dirent) + strlen(name));
+		/* record length incl. alignment. */
+                len = _DIRENT_RECLEN(dent, strlen(name));
 
 		/* Is the user buffer too small to store another record? */
 		if (user_off + off + len > user_left) {
@@ -190,8 +192,8 @@ int fs_getdents(void)
 		/* Fill in the actual directory entry. */
 		dent = (struct dirent *) &buf[off];
 		dent->d_ino = (ino_t) get_inode_number(child);
-		dent->d_off = (off_t) pos;
 		dent->d_reclen = len;
+		dent->d_type = fs_mode_to_type(node->i_stat.mode);
 		strcpy(dent->d_name, name);
 
 		off += len;

JBouron/Minix3-Fine-Grain

Change-Id: I1459755c7ba5e5d1c9396d3a587ce6e63ddc283e

lib/libvtreefs/read.c
@@ -2,6 +2,7 @@
 
 #include "inc.h"
 #include <dirent.h>
+#include <minix/minlib.h>
 
 #define GETDENTS_BUFSIZ 4096
 #define DWORD_ALIGN(len) (((len) + sizeof(long) - 1) & ~(sizeof(long) - 1))
@@ -161,7 +162,8 @@ int fs_getdents(void)
 			name = child->i_name;
 		}
 
-		len = DWORD_ALIGN(sizeof(struct dirent) + strlen(name));
+		/* record length incl. alignment. */
+                len = _DIRENT_RECLEN(dent, strlen(name));
 
 		/* Is the user buffer too small to store another record? */
 		if (user_off + off + len > user_left) {
@@ -190,8 +192,9 @@ int fs_getdents(void)
 		/* Fill in the actual directory entry. */
 		dent = (struct dirent *) &buf[off];
 		dent->d_ino = (ino_t) get_inode_number(child);
-		dent->d_off = (off_t) pos;
 		dent->d_reclen = len;
+		dent->d_type = fs_mode_to_type(child->i_stat.mode);
+		dent->d_namlen = strlen(name);
 		strcpy(dent->d_name, name);
 
 		off += len;