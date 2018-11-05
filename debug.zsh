1e3757543bc57267a88d34653fcb12694eb572fb
root <root@Thel_Vadam.localdomain>
2018-11-04 21:01:36 -0800

    Add port option to debug script.

--- debug.zsh
+++ debug.zsh
@@ -2,5 +2,9 @@
 
 KERNEL_IMAGE="obj.i386/minix/kernel/kernel"
 DEFAULT_LAYOUT="src"
-REMOTE_ADDR="localhost:1234"
+if [[ $@ > 1 ]]; then
+	PORT=$1
+else
+	PORT="1234"
+REMOTE_ADDR="localhost:$PORT"
 gdb -s $KERNEL_IMAGE -ex "target remote $REMOTE_ADDR" -ex "layout $DEFAULT_LAYOUT"
